8b30ae9ed5ec475fee1b0052d13eef8a919f971b
Kir Perepolov <johnyd@bigmir.net>
2016-10-24 16:48:17 +0300

    getDecimal

--- bin/JavaScript_tests/js/script.js
+++ bin/JavaScript_tests/js/script.js
@@ -1,17 +1,23 @@
 $("#block1").mousedown(function(){
 	
-$("#block1").mousemove(
-	function(){
-		$("#block1").css("top", event.clientY-10)
-							.css("left", event.clientX-10);	 
-	console.log("X: " + event.clientX);}
-);
+	$("#block1").mousemove(
+		function(){
+			$("#block1").css("top", event.clientY-20)
+			.css("left", event.clientX-20);	 
+	//console.log("X: " + event.clientX);
 }
 );
-$("#block1").mouseup(function(){
-			$("#block1").css("background-color", "#ff0");
-		}
+}
+).mouseup(function(){
+	console.log($("#block1").css("top"), $("#block1").css("top"));
+}
 
-	);
+);
 
 // console.log(window);
+var inputNumber = +prompt("your number", "");
+function getDecimal(numParam) {
+	numParam = (numParam < 0) ? -1*numParam : numParam;
+	return +(numParam - +numParam.toFixed(0)).toFixed(10);
+};
+$("#block1").text(getDecimal(inputNumber));
