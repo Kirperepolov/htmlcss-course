895ef98996f82f9a2dce972c83ac6e87337b2b67
Kir Perepolov <johnyd@bigmir.net>
2016-10-26 17:31:50 +0300

    jquery .on and .off

--- bin/JavaScript_tests/js/script.js
+++ bin/JavaScript_tests/js/script.js
@@ -1,23 +1,22 @@
-$("#block1").mousedown(function(){
+$("#block1").on("mousedown",drugAndDrop);
+function drugAndDrop(){
 	
 	$("#block1").mousemove(
 		function(){
 			$("#block1").css("top", event.clientY-20)
 			.css("left", event.clientX-20);	 
 	//console.log("X: " + event.clientX);
-}
-);
-}
-).mouseup(function(){
-	console.log($("#block1").css("top"), $("#block1").css("top"));
-}
-
-);
+});
+};
+$("#block1").off("mouseup", drugAndDrop);
 
 // console.log(window);
-var inputNumber = +prompt("your number", "");
-function getDecimal(numParam) {
-	numParam = (numParam < 0) ? -1*numParam : numParam;
-	return +(numParam - +numParam.toFixed(0)).toFixed(10);
-};
-$("#block1").text(getDecimal(inputNumber));
+/* function for getting the decimal part of a number
+	*
+	*		var inputNumber = +prompt("your number", "");
+	*		function getDecimal(numParam) {
+	*			numParam = (numParam < 0) ? -1*numParam : numParam;
+	*			return +(numParam - +numParam.toFixed(0)).toFixed(10);
+	*		};
+	*		$("#block1").text(getDecimal(inputNumber));
+	*/
