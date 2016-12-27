37c783b5d9cf0b1b1649e1724f2ecab75a547259
Kir Perepolov <johnyd@bigmir.net>
2016-12-27 23:13:39 +0200

    a product card task is started

---
+++ bin/prod_card/script/app.js
@@ -0,0 +1,52 @@
+(function() {
+  'use strict';
+
+  angular.module('PordCard',[])
+  .controller('ProdCardController',ProdCardController)
+  .service('GetProdService',GetProdService)
+
+  ProdCardController.$inject=['GetProdService'];
+  function ProdCardController(GetProdService){
+    var ctrl = this;
+    ctrl.product;
+
+    ctrl.find = function(searchItem){
+      if (typeof(searchItem) !== 'number') {
+        searchItem = 505795;
+      };
+
+        var foundPromise = GetProdService.getProduct(searchItem);
+        foundPromise.then(function(obj){
+          ctrl.product=obj;
+        }).catch(error=>console.log(error.message));
+
+    };
+
+  };
+
+  GetProdService.$inject = ['$http'];
+  function GetProdService($http){
+    var service = this;
+    service.product ={};
+
+    service.getProduct = function(searchId){
+      let path = '../json/product.json';
+
+      return $http.get(path)    //a shortcut method fot GET requests
+      .then(function (result) {
+        let prodList = result.data.product_items;
+
+        for (let i=0;i<prodList.length;i++){
+          if (prodList[i].id.includes(searchId)){
+            service.product = prodList[i];
+          };
+        };
+
+        return service.product;
+      })
+      .catch(error=>error.message);
+    };
+
+  };
+
+}());
